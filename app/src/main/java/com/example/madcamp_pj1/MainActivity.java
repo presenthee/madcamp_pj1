3455018313fe723a80686772a2de1ed41f199c7a
presenthee <khyunhii@gmail.com>
2021-12-28 20:37:03 +0900

    listview

--- app/src/main/java/com/example/madcamp_pj1/MainActivity.java
+++ app/src/main/java/com/example/madcamp_pj1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.madcamp_pj1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView);
+        List<String> data = new ArrayList<>();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
+
+        data.add("test1");
+        data.add("test2");
+        adapter.notifyDataSetChanged();
     }
 }
