666708b889bbfc4ba7e05cb6526f1bf7e617621a
presenthee <khyunhii@gmail.com>
2021-12-28 19:39:52 +0900

    test

--- app/src/main/java/com/example/madcamp_pj1/MainActivity.java
+++ app/src/main/java/com/example/madcamp_pj1/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.madcamp_pj1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<SampleData> movieDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.InitializeMovieData();
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        final MyAdapter myAdapter = new MyAdapter(this,movieDataList);
+
+        listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id){
+                Toast.makeText(getApplicationContext(),
+                        myAdapter.getItem(position).getMovieName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void InitializeMovieData()
+    {
+        movieDataList = new ArrayList<SampleData>();
+
+        movieDataList.add(new SampleData(1, "미션임파서블","15세 이상관람가"));
+        movieDataList.add(new SampleData(2, "아저씨","19세 이상관람가"));
+        movieDataList.add(new SampleData(3, "어벤져스","12세 이상관람가"));
     }
 }
